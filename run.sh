47a2eacc8fba1800398952b886a35fc04f7eb4bb
Chung_es <44948482+euisuk-chung@users.noreply.github.com>
2023-10-05 15:49:17 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 echo "Enter the user image_name: "
 read image_name
 
-sudo docker build -t $image_name --build-arg UNAME=$(whoami) --build-arg UID=$(id -u) --build-arg GID=$(id -g) .
+docker build -t $image_name --build-arg UNAME=$(whoami) --build-arg UID=$(id -u) --build-arg GID=$(id -g) .
